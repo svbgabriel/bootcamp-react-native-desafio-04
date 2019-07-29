79be13be6c79712b0cca1510946778854f00af55
Gabriel Batista <svbgabriel@gmail.com>
2019-07-29 20:58:53 -0300

    Configura o uso do OneSignal

--- android/app/src/main/java/com/gocommerce/MainApplication.java
+++ android/app/src/main/java/com/gocommerce/MainApplication.java
@@ -3,6 +3,7 @@ package com.gocommerce;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
 import com.microsoft.codepush.react.CodePush;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -31,6 +32,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeOneSignalPackage(),
             new CodePush(getResources().getString(R.string.reactNativeCodePush_androidDeploymentKey), getApplicationContext(), BuildConfig.DEBUG),
             new VectorIconsPackage(),
             new RNGestureHandlerPackage()
